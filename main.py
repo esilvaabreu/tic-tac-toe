c403aeee73c4a0367ca9646fb04e2a599bd0e4a6
esilvaabreu <125417994+esilvaabreu@users.noreply.github.com>
2024-10-29 10:51:02 +0000

    Update main.py
    Updating main comments in the code

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# Create a grid - done
-# how to save the values in the grid - done
-
-# how to input the values into the grid
-# check if someone has won
-
-# I can maybe work with coordinates in a grid
-
+# Importing necessary functions
 from tic_tac_toe import intro_game, play_game
 
+# Creating the initial variables
 player_1_shape, player_2_shape, coord_dict = intro_game()
 
+# Executing main function
 play_game(player_1_shape, player_2_shape, coord_dict)
